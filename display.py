ed85eb1cf7a54091543bad883f4b0ef04536541e
AllanMuhari <allanmhari087@gmail.com>
2022-09-22 00:03:32 +0300

    calender commit

---
+++ display.py
@@ -0,0 +1,7 @@
+from calendar import*
+year =int(input("Enter the Year: "))
+print(calendar(year,2,1,8,4))
+#2=characters fot the day
+#1=lines for each week
+#8=row for each month
+#4= columns for all months
